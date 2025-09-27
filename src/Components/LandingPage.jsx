50c1f66423066949455520439fbb4d1fecc12284
yashG <yrg.creation.ai@gmail.com>
2025-09-27 23:38:11 +0530

    working on landing page

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -4,7 +4,14 @@ const LandingPage = () => {
   return (
     <div>
        <div className='land-page w-full h-screen bg-gray-400 relative'>
-          <div className="land-content">
+          <video className='rotate-180 absolute top-0 left-0 w-full h-full object-cover'
+             autoPlay
+             loop
+             muted
+             playsInline
+             src="./bg-fog.mp4"
+          />
+          <div className="land-content w-full h-[200vh] bg-red-700">
           </div>
        </div>
     </div>
