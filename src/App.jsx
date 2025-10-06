6af007778474ab27d98c03b0c7fea4a8a2cf9a97
yashG <yrg.creation.ai@gmail.com>
2025-10-06 22:10:07 +0530

    gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
+import { useRef, useEffect, useState } from 'react'
+import { gsap } from "gsap";
 
 function App() {
 
+  const textRef = useRef();
+    useEffect(() => {
+    gsap.to(textRef.current, {
+         x: 100
+       });
+  }, []);
+
+
   return (
     <div className='w-full h-screen bg-black text-white'>
-      <h1 className="text-[2rem]"> hello react</h1>
+      <h1 ref={textRef} className="text-[2rem]"> hello react</h1>
       <h2> hello word </h2>
     </div>
   )
